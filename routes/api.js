73f5992dbc64db62e46e9089fafb79a247d8cbbd
Anna Chirapongse <achirapongse@gmail.com>
2021-01-08 18:45:08 -0500

    Updates for the api

--- routes/api.js
+++ routes/api.js
@@ -1,5 +1,6 @@
 const router = require('express').Router();
 const db = require("../models");
+const { Workout } = require('../models');
 
 router.get('/api/workouts', function(req, res) {
     db.Workout.find({}).then(function(results){
@@ -7,7 +8,26 @@ router.get('/api/workouts', function(req, res) {
     })
 });
 
-
+router.post("/api/workouts", ({ body }, res) => {
+    Workout.create(body)
+      .then(dbWorkout => {
+        res.json(dbWorkout);
+      })
+      .catch(err => {
+        res.status(400).json(err);
+      });
+  });
+  
+  router.post("/api/workout/add", ({ body }, res) => {
+    Workout.insertMany(body)
+      .then(dbWorkout => {
+        res.json(dbWorkout);
+      })
+      .catch(err => {
+        res.status(400).json(err);
+      });
+  });
+  
 
 
 module.exports = router;
