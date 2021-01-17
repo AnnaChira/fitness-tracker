9ef1b2ac147898e61b7c22f0236cb33cad924ee1
Anna Chirapongse <achirapongse@gmail.com>
2021-01-17 15:48:35 -0500

    Updating for the heroku and database

--- server.js
+++ server.js
@@ -10,14 +10,15 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
-  useNewUrlParser: true,
-  useFindAndModify: false
-});
-
-var MONGODB_URI = process.env.MONGODB_URI
-
-mongoose.connect(MONGODB_URI);
+mongoose.connect(
+  process.env.MONGODB_URI || 'mongodb://localhost/budget',
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false
+  }
+)
 
 // routes
 app.use(require("./routes/api.js"));
